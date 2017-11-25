187b6234507657726d124c5f5f764bad005f15e8
Casey Poon <caseycichi>
2017-11-25 00:37:39 -0800

    adding changes

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendList = [
+  {
+    name: 'Sassy Cassey',
+    profilePic: 'https://www.facebook.com/photo.php?fbid=1129496470495805&set=a.110792125699583.16135.100003063873411&type=3&theater',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
